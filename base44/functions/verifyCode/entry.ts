540606535d4cb51ce4f2c4a077ef21ab220bc440
Brendan Byrne <brendan@shadowwolvesproductions.com.au>
2026-04-15 02:42:20 +0000

    Manual code change

--- base44/functions/verifyCode/entry.ts
+++ base44/functions/verifyCode/entry.ts
@@ -65,7 +65,7 @@ Deno.serve(async (req) => {
     ]);
 
     const newScore = calculateCineScore(updatedProfile, endorsements.length, castingApps.length);
-    updateData.cine_score = newScore;
+    updateData.spot_score = newScore;
 
     await base44.asServiceRole.entities.Profile.update(profile.id, updateData);
   }
